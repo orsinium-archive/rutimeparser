6f3b550bd7cb5e0f9c509d953322c4bcb37f089e
orsinium <master_fess@mail.ru>
2018-02-10 12:28:59 +0500

    result getting improvements

--- rutimeparser/core.py
+++ rutimeparser/core.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, date, time
 
 from .get_words import get_words
 from .get_cat import get_cat
@@ -11,6 +11,9 @@ class TimeParser(object):
     Класс для получения из текста на естественном языке даты и времени.
     Возвращает datetime, date или None.
     """
+
+    allowed_results = (datetime, date, time, None)
+    default_time = time(9, 0)
 
     def __init__(self, text='', words=None, tz=None, now=None):
         self.nodes = []
@@ -108,23 +111,61 @@ class TimeParser(object):
         """
         Возвращает словарь "категория_ноды: значение_ноды"
         """
+        if not self.nodes:
+            self.make_nodes()
         return {node.cat: node.value for node in self.nodes}
 
-    def get_datetime(self):
+    def parse(self):
         """
         Возвращает результат на основе обработанных нод
         """
         nodes = self.to_dict()
-        if 'datetime' in nodes:
-            return change_timezone(nodes['datetime'], self.tz)
-        now = datetime.now()
-        if 'date' in nodes and 'time' in nodes:
-            dt = datetime.combine(nodes['date'], nodes['time'])
-            return change_timezone(dt, self.tz)
-        if 'time' in nodes:
-            return datetime.combine(now.date(), nodes['time'])
-        if 'date' in nodes:
-            return nodes['date']
+
+        # получаем значения нод, отвечающих за дату и время
+        result_datetime = nodes.get('datetime')
+        result_date = nodes.get('date')
+        result_time = nodes.get('time')
+
+        if result_datetime is None:
+            if result_date is not None and result_time is not None:
+                result_datetime = datetime.combine(result_date, result_time)
+
+        # цепляем часовой пояс
+        if result_datetime is not None:
+            result_datetime = change_timezone(result_datetime, self.tz)
+        if result_date is not None:
+            result_date = change_timezone(result_date, self.tz)
+        if result_time is not None:
+            result_time = change_timezone(result_time, self.tz)
+
+        # возвращаем ожидаемый результат, если такой есть
+        if datetime in self.allowed_results and result_datetime is not None:
+            return result_datetime
+        if date in self.allowed_results and result_date is not None:
+            return result_date
+        if time in self.allowed_results and result_time is not None:
+            return result_time
+
+        # ищем адаптированный результат
+        if datetime in self.allowed_results and result_time is not None:
+            result = datetime.combine(self.now.date(), result_time)
+            return change_timezone(result, self.tz)
+        if datetime in self.allowed_results and result_date is not None:
+            result = datetime.combine(result_date, self.default_time)
+            return change_timezone(result, self.tz)
+
+        # возвращаем результат по умолчанию
+        if None not in self.allowed_results:
+            if datetime in self.allowed_results:
+                return self.now
+            if date in self.allowed_results:
+                return self.now.date()
+            if time in self.allowed_results:
+                return self.now.time()
+            raise KeyError('Не найден результат, соответствующий значению allowed_results')
+
+    def get_datetime(self):
+        return self.parse()
 
     def get_clear_text(self):
         result = []
